6886d55e377e5fa5b78413f58f7fd531a7b3a8f2
Danylo Goncharov <danylo.goncharov@xing.com>
2017-08-11 22:10:30 +0200

    Main setup

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/MainEntryPoint.kt
@@ -0,0 +1,5 @@
+package com.daniel.goncharov.algorithm.playground
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/tests.kt
@@ -0,0 +1,9 @@
+package com.daniel.goncharov.algorithm.playground
+
+import org.junit.Test
+
+class TestSource {
+    @Test fun f() {
+
+    }
+}
