58cdbe3b2fe63a285d90a0fd4e51c3f347c6e3b3
Danylo Goncharov <danylo.goncharov@xing.com>
2018-07-24 22:13:48 +0200

    Binary search implementation

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/binary/search/BinarySearch.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/binary/search/BinarySearch.java
@@ -6,30 +6,22 @@ public class BinarySearch {
 
     private static final int NOT_FOUND = -1;
 
-    int find(List<Integer> array, Integer element) {
+    int find(List<Integer> array, int element) {
         int left = 0;
         int right = array.size() - 1;
-        int middle = array.size() / 2;
-        while (right - left > 1) {
-            if (array.get(middle).equals(element)) {
+        int middle;
+        while (left<=right) {
+            middle = (left + right) / 2;
+            if (array.get(middle)==element) {
                 return middle;
             } else if (array.get(middle) > element) {
                 right = middle - 1;
-                middle = (left + right) / 2;
             } else if (array.get(middle) < element) {
                 left = middle + 1;
-                middle = (left + right) / 2;
+
             }
         }
-        Integer leftElement = array.get(left);
-        Integer rightElement = array.get(right);
-        if (element.equals(leftElement)) {
-            return left;
-        } else if (element.equals(rightElement)) {
-            return right;
-        } else {
-            return NOT_FOUND;
-        }
+        return NOT_FOUND;
     }
 
 }
--- src/test/kotlin/com/daniel/goncharov/algorithm/playground/binary/search/BinarySearchTest.kt
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/binary/search/BinarySearchTest.kt
@@ -8,10 +8,17 @@ import org.junit.Test
 class BinarySearchTest {
 
     @Test
-    fun shouldSortEven() {
+    fun shouldFindGeneric() {
         val sortedList = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         val expectedPosition = BinarySearch().find(sortedList, 8)
         assertThat(expectedPosition).isEqualTo(8)
+    }
+
+    @Test
+    fun shouldFindViaTwo() {
+        val sortedList = listOf(0, 1)
+        val expectedPosition = BinarySearch().find(sortedList, 1)
+        assertThat(expectedPosition).isEqualTo(1)
     }
 
 }
