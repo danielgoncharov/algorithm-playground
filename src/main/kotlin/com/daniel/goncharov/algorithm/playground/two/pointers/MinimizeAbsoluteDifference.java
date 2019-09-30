b9d522c124b78d8c48f0e7b636332a4e1b62e933
Danylo Goncharov <danylo.goncharov@xing.com>
2019-09-30 22:46:35 +0200

    Wip

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/two/pointers/MinimizeAbsoluteDifference.java
@@ -0,0 +1,26 @@
+package com.daniel.goncharov.algorithm.playground.two.pointers;
+
+import java.util.ArrayList;
+
+public class MinimizeAbsoluteDifference {
+
+    public int solve(ArrayList<Integer> arrayA, ArrayList<Integer> arrayB, ArrayList<Integer> arrayC) {
+        int min = Integer.MAX_VALUE;
+        int indexA = 0;
+        int indexB = 0;
+        int indexC = 0;
+        while (indexA < arrayA.size() && indexB < arrayB.size() && indexC < arrayC.size()) {
+            int elementA = arrayA.get(indexA);
+            int elementB = arrayB.get(indexB);
+            int elementC = arrayC.get(indexC);
+            int minValue = Math.min(Math.min(elementA, elementB), elementC);
+            int tryMin = Math.abs(Math.max(Math.max(elementA, elementB), elementC) - minValue);
+            if (tryMin < min) min = tryMin;
+            if (minValue == elementA) indexA++;
+            if (minValue == elementB) indexB++;
+            if (minValue == elementC) indexC++;
+        }
+        return min;
+
+    }
+}
---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/two/pointers/IntersectionSortedArraysTest.kt
@@ -0,0 +1,12 @@
+package com.daniel.goncharov.algorithm.playground.two.pointers
+
+import org.junit.Test
+
+class IntersectionSortedArraysTest {
+    @Test
+    fun trivialIntersectionTest() {
+        val left = arrayListOf(1, 3, 8, 10, 13, 13, 16, 16, 16, 18, 21, 23, 24, 31, 31, 31, 33, 35, 35, 37, 37, 38, 40, 41, 43, 47, 47, 48, 48, 52, 52, 53, 53, 55, 56, 60, 60, 61, 61, 63, 63, 64, 66, 67, 67, 68, 69, 71, 80, 80, 80, 80, 80, 80, 81, 85, 87, 87, 88, 89, 90, 94, 95, 97, 98, 98, 100, 101)
+        val right = arrayListOf(5, 7, 14, 14, 25, 28, 28, 34, 35, 38, 38, 39, 46, 53, 65, 67, 69, 70, 78, 82, 94, 94, 98)
+        IntersectionSortedArrays().intersect(left, right)
+    }
+}
