8351571d455465705522f956f5b8c99d3cb620bc
Danylo Goncharov <danylo.goncharov@xing.com>
2019-10-08 22:25:01 +0200

    Diffk

---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/two/pointers/DiffkTest.kt
@@ -0,0 +1,14 @@
+package com.daniel.goncharov.algorithm.playground.two.pointers
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class DiffkTest {
+
+    @Test
+    fun trivialTest() {
+        val expected = Diffk().diffPossible(arrayListOf(1, 2, 2, 3, 4), 0)
+        assertThat(expected).isEqualTo(1)
+    }
+}
