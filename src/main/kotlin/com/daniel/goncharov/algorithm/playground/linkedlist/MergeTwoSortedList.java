00e023dc6ac7451e88e95851027ee0eeb02729d4
Danylo Goncharov <danylo.goncharov@xing.com>
2019-10-29 21:48:33 +0100

    merge better version

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/linkedlist/MergeTwoSortedList.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/linkedlist/MergeTwoSortedList.java
@@ -1,40 +1,22 @@
 package com.daniel.goncharov.algorithm.playground.linkedlist;
 
 public class MergeTwoSortedList {
-    ListNode result = null;
-    ListNode currentNode = null;
 
     public ListNode mergeTwoLists(ListNode listA, ListNode listB) {
-
+        final ListNode head =  new ListNode(0);
+        ListNode prev = head;
         while (listA != null || listB != null) {
-            if (listA == null) {
-                addToResult(listB.val);
-                listB = listB.next;
-                continue;
-            }
-            if (listB == null) {
-                addToResult(listA.val);
-                listA = listA.next;
-                continue;
-            }
-            if (listA.val < listB.val) {
-                addToResult(listA.val);
+            boolean moveA = listB == null || (listA != null && listA.val <= listB.val);
+            if (moveA) {
+                prev.next = listA;
                 listA = listA.next;
             } else {
-                addToResult(listB.val);
+                prev.next = listB;
                 listB = listB.next;
             }
+            prev = prev.next;
+            prev.next = null;
         }
-        return result;
-    }
-
-    private void addToResult(int val) {
-        if (result == null) {
-            result = new ListNode(val);
-            currentNode = result;
-        } else {
-            currentNode.next = new ListNode(val);
-            currentNode = currentNode.next;
-        }
+        return head.next;
     }
 }
