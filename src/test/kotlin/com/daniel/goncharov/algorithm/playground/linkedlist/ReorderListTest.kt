d7bfad64c24633daddd87e17720846daf60387cd
Danylo Goncharov <danylo.goncharov@xing.com>
2019-10-31 16:36:49 +0100

    reorder list

---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/linkedlist/ReorderListTest.kt
@@ -0,0 +1,11 @@
+package com.daniel.goncharov.algorithm.playground.linkedlist
+
+import org.junit.Test
+
+class ReorderListTest {
+    @Test
+    fun test1() {
+        val listA = arrayListOf(1, 2, 3, 4,5,6,7,8,9).toLinkedList()
+        val result = ReorderList().reorderList(listA)
+    }
+}
