24f96f61ca748e783ec5991ce9b6b8f346cef6c7
Danylo Goncharov <danylo.goncharov@xing.com>
2019-11-03 11:17:37 +0100

    wip

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/linkedlist/KReverseLinkedList.java
@@ -0,0 +1,51 @@
+package com.daniel.goncharov.algorithm.playground.linkedlist;
+
+public class KReverseLinkedList {
+
+    public ListNode reverseList(ListNode input, int reverseInterval) {
+        if (reverseInterval == 1) return input;
+        ListNode head = new ListNode(0);
+        ListNode previous = null;
+        ListNode current = input;
+        ListNode next = input.next;
+        ListNode intervalHead = null;
+        ListNode intervalTail = null;
+        int index = 1;
+        while (current != null) {
+            if (isTail(reverseInterval, index)) {
+                if (intervalTail != null) {
+                    intervalHead = intervalTail;
+                }
+                intervalTail = current;
+            }
+            if (isHead(reverseInterval, index)) {
+                if (intervalHead != null) {
+                    intervalHead.next = current;
+                }
+                intervalHead = current;
+            }
+            if (isHead(reverseInterval, index) && head.next == null) {
+                head.next = current;
+            }
+
+            current.next = previous;
+            previous = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+            index++;
+
+        }
+        intervalTail.next = null;
+        return head.next;
+    }
+
+    private boolean isHead(int reverseInterval, int index) {
+        return index % reverseInterval == 0;
+    }
+
+    private boolean isTail(int reverseInterval, int index) {
+        return index % reverseInterval == 1;
+    }
+}
