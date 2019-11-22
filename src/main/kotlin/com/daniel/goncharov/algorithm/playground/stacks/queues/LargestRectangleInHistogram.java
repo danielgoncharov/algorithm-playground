6dffdebf72138ea89768f288722df5aa02f0c656
Danylo Goncharov <danylo.goncharov@xing.com>
2019-11-22 22:05:25 +0100

    Largest Rectangle in Histogram

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/stacks/queues/LargestRectangleInHistogram.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/stacks/queues/LargestRectangleInHistogram.java
@@ -8,17 +8,26 @@ public class LargestRectangleInHistogram {
     public int largestRectangleArea(ArrayList<Integer> histograms) {
         Stack<Integer> indexesStack = new Stack<>();
         int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for (int index = 0; index < histograms.size(); index++) {
             int currentElement = histograms.get(index);
-            int stackTop = histograms.get(indexesStack.peek());
-            while (!indexesStack.isEmpty() && currentElement < stackTop) {
-                stackTop = indexesStack.pop();
+            if (currentElement < min) min = currentElement;
+            while (!indexesStack.isEmpty() && currentElement < histograms.get(indexesStack.peek())) {
+                int stackTopIndex = indexesStack.pop();
                 int rightIndex = index;
                 int leftIndex = indexesStack.isEmpty() ? -1 : indexesStack.peek();
-                max = Math.max(max, (rightIndex - leftIndex - 1) * stackTop);
+                max = Math.max(max, (rightIndex - leftIndex - 1) * histograms.get(stackTopIndex));
             }
             indexesStack.push(index);
         }
+        max = Math.max(min * histograms.size(), max);
+        int size = histograms.size();
+        while (!indexesStack.isEmpty()) {
+            int currentIndex = indexesStack.pop();
+            int area = histograms.get(currentIndex) * (indexesStack.empty() ? size : size - indexesStack.peek() - 1);
+            max= Math.max(area, max);
+        }
         return max;
+
     }
 }
---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/stacks/queues/LargestRectangleInHistogramTest.kt
@@ -0,0 +1,58 @@
+package com.daniel.goncharov.algorithm.playground.stacks.queues
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class LargestRectangleInHistogramTest {
+
+    private val largestRectangleInHistogram = LargestRectangleInHistogram()
+
+    @Test
+    fun shouldCalculateTrivial() {
+        val list = arrayListOf(2, 1, 5, 6, 2, 3)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(10)
+    }
+
+    @Test
+    fun shouldCalculateUniformWithOnes() {
+        val list = arrayListOf(1, 1, 1, 1, 1, 1)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(6)
+    }
+
+    @Test
+    fun shouldCalculateUniformWithFour() {
+        val list = arrayListOf(4, 4, 4, 4, 4, 4)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(24)
+    }
+
+    @Test
+    fun shouldCalculateTower() {
+        val list = arrayListOf(4, 200, 4, 4, 4, 4)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(200)
+    }
+
+    @Test
+    fun shouldCalculateTwoTowers() {
+        val list = arrayListOf(6, 2, 2, 2, 2, 6)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(12)
+    }
+
+    @Test
+    fun shouldCalculateAsc() {
+        val list = arrayListOf(2, 3, 4, 5, 6, 7)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(16)
+    }
+
+    @Test
+    fun shouldCalculateDesc() {
+        val list = arrayListOf(7, 6, 5, 4, 3, 2)
+        val result = largestRectangleInHistogram.largestRectangleArea(list)
+        assertThat(result).isEqualTo(16)
+    }
+}
