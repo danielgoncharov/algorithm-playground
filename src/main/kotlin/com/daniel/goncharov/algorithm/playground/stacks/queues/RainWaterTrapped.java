a35f0d1e321e37a319bf4a6b75f45c24d0bb7e86
Danylo Goncharov <danylo.goncharov@xing.com>
2019-11-28 20:51:37 +0100

    Watter level trapped

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/stacks/queues/RainWaterTrapped.java
@@ -0,0 +1,36 @@
+package com.daniel.goncharov.algorithm.playground.stacks.queues;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class RainWaterTrapped {
+
+    public int trap(final List<Integer> levels) {
+        Stack<Integer> picksStack = new Stack<>();
+        int watterAmount = getLevel(levels, picksStack);
+        if (picksStack.isEmpty()) return watterAmount;
+        List<Integer> leftovers = new ArrayList<>();
+        while (!picksStack.isEmpty()) leftovers.add(picksStack.pop());
+        return getLevel(leftovers, new Stack<>()) + watterAmount;
+    }
+
+    private int getLevel(final List<Integer> levels, Stack<Integer> picksStack) {
+        int trappedWater = 0;
+        int lastMax = 0;
+        for (Integer level : levels) {
+            if (picksStack.isEmpty()) {
+                lastMax = level;
+            } else if (level >= lastMax) {
+                while (picksStack.peek() != lastMax) {
+                    trappedWater += Math.min(lastMax, level) - picksStack.pop();
+                }
+                picksStack.pop();
+                lastMax = level;
+            }
+            picksStack.push(level);
+
+        }
+        return trappedWater;
+    }
+}
---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/stacks/queues/RainWaterTrappedTest.kt
@@ -0,0 +1,17 @@
+package com.daniel.goncharov.algorithm.playground.stacks.queues
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class RainWaterTrappedTest {
+
+    private val rainWaterTrapped = RainWaterTrapped()
+
+    @Test
+    fun shouldCalculateTrivial() {
+        val list = arrayListOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)
+        val result = rainWaterTrapped.trap(list)
+        Assertions.assertThat(result).isEqualTo(6)
+    }
+
+}
