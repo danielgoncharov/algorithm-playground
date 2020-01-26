b73281945ca90869414e80a05ff9fe3e4ff6934d
Danylo Goncharov <danylo.goncharov@xing.com>
2020-01-26 18:39:55 +0100

    nqueens

---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/backtracking/NQueensTest.kt
@@ -0,0 +1,12 @@
+package com.daniel.goncharov.algorithm.playground.backtracking
+
+import org.junit.Test
+
+class NQueensTest {
+
+    @Test
+    fun shouldFindGeneric() {
+        val result = NQueens().solveNQueens(5)
+    }
+
+}
