e787f4d485cdaf8cd25e37bd9b80f6e24820bf2e
Danylo Goncharov <danylo.goncharov@xing.com>
2020-01-27 21:31:43 +0100

    Sudoku first draft

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/backtracking/Sudoku.java
@@ -0,0 +1,79 @@
+package com.daniel.goncharov.algorithm.playground.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sudoku {
+
+    private boolean isSolutionFound = false;
+
+    public void solveSudoku(ArrayList<ArrayList<Character>> board) {
+        backtrack(board, 0, 0);
+    }
+
+    private void backtrack(
+            ArrayList<ArrayList<Character>> board,
+            int rowStart,
+            int columnStart) {
+        if (rowStart == board.size() - 1 && columnStart == board.get(rowStart).size() - 1) {
+            isSolutionFound = true;
+            return;
+        }
+        for (int rowIndex = rowStart; rowIndex < board.size(); rowIndex++) {
+            if (isSolutionFound) return;
+            ArrayList<Character> row = board.get(rowIndex);
+            for (int columnIndex = columnStart; columnIndex < row.size(); columnIndex++) {
+                if (isSolutionFound) return;
+                Character character = row.get(columnIndex);
+                if (character != '.') continue;
+                for (int number = 1; number < 10; number++) {
+                    if (!isUnique(number, board, rowIndex, columnIndex)) continue;
+                    row.set(columnIndex, (char) ('0' + number));//TODO check cast to char
+                    backtrack(board, rowIndex, columnIndex);
+                    if (isSolutionFound) return;
+                    row.set(columnIndex, '.');
+                }
+            }
+        }
+    }
+
+    private boolean isUnique(int number,
+                             ArrayList<ArrayList<Character>> board,
+                             int rowIndex,
+                             int columnIndex) {
+        List<Character> numbersMap = new ArrayList<>();
+        numbersMap.add((char) number);
+        ArrayList<Character> row = board.get(rowIndex);
+        for (int index = 0; index < row.size(); index++) {
+            Character character = row.get(index);
+            if (character == '.') continue;
+            if (numbersMap.contains(character)) return false;
+            numbersMap.add(character);
+        }
+        numbersMap.clear();
+        numbersMap.add((char) number);
+        for (int index = 0; index < row.size(); index++) {
+            Character character = board.get(index).get(columnIndex);
+            if (character == '.') continue;
+            if (numbersMap.contains(character)) return false;
+            numbersMap.add(character);
+        }
+        numbersMap.clear();
+        numbersMap.add((char) number);
+
+        int startRowIndex = (rowIndex / 3) * 3;
+        int startColumnIndex = (columnIndex / 3) * 3;
+        for (int rowI = startRowIndex; rowI < startRowIndex + 3; rowI++) {
+            row = board.get(rowI);
+            for (int columnI = startColumnIndex; columnI < startColumnIndex + 3; columnI++) {
+                Character character = row.get(columnI);
+                if (character == '.') continue;
+                if (numbersMap.contains(character)) return false;
+                numbersMap.add(character);
+            }
+        }
+
+        return true;
+    }
+
+}
---
+++ src/test/kotlin/com/daniel/goncharov/algorithm/playground/backtracking/SudokuTest.kt
@@ -0,0 +1,21 @@
+package com.daniel.goncharov.algorithm.playground.backtracking
+
+import org.junit.Test
+
+class SudokuTest {
+    @Test
+    fun shouldFindGeneric1() {
+        val input = arrayListOf(
+                arrayListOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+                arrayListOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+                arrayListOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+                arrayListOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+                arrayListOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+                arrayListOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+                arrayListOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+                arrayListOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+                arrayListOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+        val result = Sudoku().solveSudoku(input)
+    }
+}
