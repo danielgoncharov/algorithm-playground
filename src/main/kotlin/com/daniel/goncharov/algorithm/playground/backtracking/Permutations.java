75213bd68a85519b6fd319c33f02f081ba1d0efe
Danylo Goncharov <danylo.goncharov@xing.com>
2020-02-05 21:07:06 +0100

    Permutation

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/backtracking/Permutations.java
@@ -0,0 +1,28 @@
+package com.daniel.goncharov.algorithm.playground.backtracking;
+
+import java.util.ArrayList;
+
+public class Permutations {
+
+    public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> numbers) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        backtrack(new ArrayList<>(), result, numbers);
+        return result;
+    }
+
+    private void backtrack(ArrayList<Integer> permutation,
+                           ArrayList<ArrayList<Integer>> result,
+                           ArrayList<Integer> numbers) {
+        if (permutation.size() == numbers.size()) {
+            result.add(new ArrayList<>(permutation));
+            return;
+        }
+        for (int index = 0; index < numbers.size(); index++) {
+            Integer currentElement = numbers.get(index);
+            if (permutation.contains(currentElement)) continue;
+            permutation.add(currentElement);
+            backtrack(permutation, result, numbers);
+            permutation.remove(permutation.size() - 1);
+        }
+    }
+}
