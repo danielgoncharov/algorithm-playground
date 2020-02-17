fb10577aff4ed9fd455edee649f298d92752d84c
Danylo Goncharov <danylo.goncharov@xing.com>
2020-02-17 21:42:18 +0100

    sum2

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/Sum2.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/Sum2.java
@@ -6,33 +6,25 @@ import java.util.List;
 
 public class Sum2 {
 
-    public ArrayList<Integer> twoSum(final List<Integer> numbers, int targetNumber) {
-        HashMap<Integer, Integer> numbersMap = new HashMap<>();
-        for (int index = 0; index < numbers.size(); index++) {
-            int currentNumber = numbers.get(index);
-            if (!numbersMap.containsKey(currentNumber)) numbersMap.put(currentNumber, index);
-        }
+    public ArrayList<Integer> twoSum(final List<Integer> numbers, int sum2Goal) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         ArrayList<Integer> result = new ArrayList<>();
-        if (numbersMap.size() == 1 && numbers.size() > 1) {
-            result.add(1);
-            result.add(2);
-            return result;
-        }
-        int leftIndex = Integer.MAX_VALUE;
-        int rightIndex = Integer.MAX_VALUE;
+
         for (int index = 0; index < numbers.size(); index++) {
-            int currentNumber = numbers.get(index);
-            int numberToLook = targetNumber - currentNumber;
-            Integer foundIndex = numbersMap.get(numberToLook);
-            if (foundIndex == null || index == foundIndex || index > foundIndex) continue;
-            if (foundIndex < rightIndex || (foundIndex == rightIndex && index < leftIndex)) {
-                result.clear();
+            int curr = numbers.get(index);
+
+            if (map.containsKey(sum2Goal - curr)) {
+                Integer foundIndex = map.get(sum2Goal - curr);
+                result.add(foundIndex);
                 result.add(index + 1);
-                result.add(foundIndex + 1);
-                leftIndex = index;
-                rightIndex = foundIndex;
+                return result;
+            } else if (!map.containsKey(curr)) {
+                map.put(curr, index + 1);
             }
         }
+
         return result;
     }
 }
