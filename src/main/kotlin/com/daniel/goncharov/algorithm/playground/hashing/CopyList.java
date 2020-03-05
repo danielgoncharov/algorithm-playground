5ec9f6d142671004694aa2adc04c8a9a803d67b4
Danylo Goncharov <danylo.goncharov@xing.com>
2020-03-05 21:12:09 +0100

    Copy list

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/CopyList.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/CopyList.java
@@ -1,15 +1,29 @@
 package com.daniel.goncharov.algorithm.playground.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopyList {
 
     public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
         RandomListNode deepCopyPointer = new RandomListNode(0);
-        RandomListNode copyNode = new RandomListNode(head.label);
-        deepCopyPointer.next = copyNode;
-        RandomListNode current = head.next;
+        RandomListNode copyNode = deepCopyPointer;
+        RandomListNode current = head;
         while (current != null) {
-            copyNode.next = new RandomListNode(current.label);
+            if (map.containsKey(current)) {
+                copyNode.next = map.get(current);
+            } else {
+                copyNode.next = new RandomListNode(current.label);
+                map.put(current, copyNode.next);
+            }
             copyNode = copyNode.next;
+            if (map.containsKey(current.random)) {
+                copyNode.random = map.get(current.random);
+            } else if (current.random != null) {
+                copyNode.random = new RandomListNode(current.random.label);
+                map.put(current.random, copyNode.random);
+            }
             current = current.next;
         }
         return deepCopyPointer.next;
--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/RandomListNode.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/RandomListNode.java
@@ -2,6 +2,9 @@ package com.daniel.goncharov.algorithm.playground.hashing;
 
 public class RandomListNode {
     int label;
-    RandomListNode next, random;
-    RandomListNode(int x) { this.label = x; }
+    public RandomListNode next, random;
+
+    public RandomListNode(int x) {
+        this.label = x;
+    }
 }
