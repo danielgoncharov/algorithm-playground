a85074637c5a32557bdab4e7ae55cd796be3bdfb
Danylo Goncharov <danylo.goncharov@xing.com>
2020-03-16 20:17:33 +0100

    Fraction

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/Fraction.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/Fraction.java
@@ -1,66 +1,26 @@
 package com.daniel.goncharov.algorithm.playground.hashing;
 
-    import java.math.BigDecimal;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Fraction {
 
-    public String fractionToDecimal(int numIn, int denIn) {
-        long num = numIn;
-        long den = denIn;
-        int sign = ((num > 0 && den > 0) || (num < 0 && den < 0)) ? 1 : -1;
-        num = Math.abs(num);
-        den = Math.abs(den);
-        if (num == 0) return "0";
-        if (den == 1) return String.valueOf((num / den) * sign);
-        if (canDiv2or5(den)) return new BigDecimal(sign * (num / (double) den)).toPlainString();
-        long floorPart = num / den;
-        num = num % den;
-        LinkedHashMap<Long, String> linkedHashMap = new LinkedHashMap<>();
-        String result = floorPart + ".";
-        while (!linkedHashMap.containsKey(num)) {
-            long numNew = num;
-            StringBuilder value = new StringBuilder();
-            while (numNew < den) {
-                numNew *= 10;
-                value.append("0");
-            }
-            value.replace(0, 1, "");
-            linkedHashMap.put(num, value.append(numNew / den).toString());
-            num = numNew % den;
+    public String fractionToDecimal(int numeratorIn, int denominatorIn) {
+        long numerator = numeratorIn, denominator = denominatorIn;
+        if (numerator % denominator == 0) return String.valueOf(numerator / denominator);
+        Map<Long, Integer> map = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+        if ((numerator > 0 && denominator < 0) || (numerator < 0 && denominator > 0)) result.append("-");
+        numerator = Math.abs(numerator);
+        denominator = Math.abs(denominator);
+        result.append(numerator / denominator + ".");
+        numerator = (numerator % denominator) * 10;
+        while (!map.containsKey(numerator)) {
+            map.put(numerator, result.length());
+            result.append(numerator / denominator);
+            numerator = (numerator % denominator) * 10;
+            if (numerator == 0) return result.toString();
         }
-
-        boolean isStaticPartAdded = false;
-        for (Map.Entry<Long, String> entry : linkedHashMap.entrySet()) {
-            Long key = entry.getKey();
-            String value = entry.getValue();
-            if (key != num && !isStaticPartAdded) {
-                result += value;
-                continue;
-            }
-            if (!isStaticPartAdded) {
-                result += "(";
-                isStaticPartAdded = true;
-            }
-            result += value;
-        }
-        if (sign == -1) {
-            result = "-" + result;
-        }
-        return result + ")";
-    }
-
-    private boolean canDiv2or5(long number) {
-        while (number != 1) {
-            if (number % 2 == 0) {
-                number /= 2;
-            } else if (number % 5 == 0) {
-                number /= 5;
-            } else {
-                return false;
-            }
-        }
-        return true;
+        return result.insert(map.get(numerator), "(").append(")").toString();
     }
 }
