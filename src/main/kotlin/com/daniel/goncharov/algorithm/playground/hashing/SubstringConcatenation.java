f270fe9e219961bf7bb90c32431e21538fbf0987
Danylo Goncharov <danylo.goncharov@xing.com>
2020-03-17 22:04:01 +0100

    SubstringConcatenation

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/SubstringConcatenation.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/hashing/SubstringConcatenation.java
@@ -15,31 +15,31 @@ public class SubstringConcatenation {
             piecesMap.put(piece, count);
         }
         int windowSize = pieces.get(0).length();
-        int startIndex = 0;
         int matchCount = 0;
         ArrayList<Integer> result = new ArrayList<>();
         HashMap<String, Integer> checkMap = new HashMap<>();
-        for (int index = 0; index < string.length(); ) {
-            int lastIndex = index + windowSize > string.length() ? string.length() : index + windowSize;
-            String windowedString = string.substring(index, lastIndex);
-            if (checkMap.isEmpty()) startIndex = index;
-            int count = checkMap.getOrDefault(windowedString, 0);
-            count++;
-            checkMap.put(windowedString, count);
-            if (piecesMap.containsKey(windowedString)
-                    && checkMap.get(windowedString) <= piecesMap.get(windowedString)) {
-                matchCount++;
-                if (matchCount == pieces.size()) {
-                    checkMap.clear();
-                    result.add(startIndex);
-                    matchCount = 0;
+        int checkSize = pieces.size() * windowSize;
+        for (int j = 0; j <= string.length() - checkSize; j++) {
+            String substringToCheckPermutation = string.substring(j, j + checkSize);
+            for (int index = 0; index < substringToCheckPermutation.length(); index += windowSize) {
+                String windowedString = substringToCheckPermutation.substring(index, index + windowSize);
+                int count = checkMap.getOrDefault(windowedString, 0);
+                count++;
+                checkMap.put(windowedString, count);
+                if (piecesMap.containsKey(windowedString)
+                        && checkMap.get(windowedString) <= piecesMap.get(windowedString)) {
+                    matchCount++;
+                    if (matchCount == pieces.size()) {
+                        checkMap.clear();
+                        result.add(j);
+                        matchCount = 0;
+                        break;
+                    }
                 } else {
-                    index += windowSize;
+                    checkMap.clear();
+                    matchCount = 0;
+                    break;
                 }
-            } else {
-                checkMap.clear();
-                index++;
-                matchCount = 0;
             }
         }
         return result;
