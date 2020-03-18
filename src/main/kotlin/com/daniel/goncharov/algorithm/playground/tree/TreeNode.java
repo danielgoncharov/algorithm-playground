a9da131bd2cfb2d451ee20577a9e6708ce3ca2d1
Danylo Goncharov <danylo.goncharov@xing.com>
2020-03-18 15:33:24 +0100

    ValidBinarySearchTree

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.daniel.goncharov.algorithm.playground.tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
