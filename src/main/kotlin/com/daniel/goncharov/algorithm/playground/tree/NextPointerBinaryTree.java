03748540f16925db053eea8551ef86dba3274fab
Danylo Goncharov <danylo.goncharov@xing.com>
2020-03-24 20:34:04 +0100

    NextPointerBinaryTree shorter version

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/NextPointerBinaryTree.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/NextPointerBinaryTree.java
@@ -6,38 +6,29 @@ import java.util.LinkedList;
 public class NextPointerBinaryTree {
 
     public void connect(TreeLinkNode root) {
-        Deque<Pair> deque = new LinkedList();
-        int level = 0;
-        TreeLinkNode currentNode = root;
-        if (currentNode.left != null) {// is last level
-            deque.add(new Pair(currentNode.left, level + 1));
-            deque.add(new Pair(currentNode.right, level + 1));
-        }
+        int index = 1;
+        int levelSeparator = 1;
+
+        Deque<TreeLinkNode> deque = new LinkedList();
+        deque.add(root);
+
+        TreeLinkNode currentNode = null;
         while (!deque.isEmpty()) {
-            Pair topOfDeque = deque.poll();
-            if (topOfDeque.node.left != null) {// is last level
-                deque.add(new Pair(topOfDeque.node.left, topOfDeque.level + 1));
-                deque.add(new Pair(topOfDeque.node.right, topOfDeque.level + 1));
+            TreeLinkNode topOfDeque = deque.poll();
+            if (topOfDeque.left != null) {// is not last level
+                deque.add(topOfDeque.left);
+                deque.add(topOfDeque.right);
             }
-            if (topOfDeque.level != level) {
-                currentNode = topOfDeque.node;
-                level = topOfDeque.level;
-                continue;
+            if (currentNode == null) {//root
+                currentNode = topOfDeque;
+            } else if (index / levelSeparator == 1) {//is on the same level
+                currentNode.next = topOfDeque;
+                currentNode = topOfDeque;
+            } else {//different levels
+                currentNode = topOfDeque;
+                levelSeparator *= 2;
             }
-            //same level
-            currentNode.next = topOfDeque.node;
-            currentNode = topOfDeque.node;
-        }
-    }
-
-    static class Pair {
-        final TreeLinkNode node;
-        final int level;
-
-        public Pair(TreeLinkNode treeLinkNode, int level) {
-
-            this.node = treeLinkNode;
-            this.level = level;
+            index++;
         }
     }
 }
