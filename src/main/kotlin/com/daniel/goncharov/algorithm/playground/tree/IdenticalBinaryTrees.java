efd5bd14dea3ff1108973dafb3d3bda70a213cf0
Danylo Goncharov <danylo.goncharov@xing.com>
2020-04-01 08:57:10 +0200

    IdenticalBinaryTrees

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/IdenticalBinaryTrees.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/IdenticalBinaryTrees.java
@@ -1,26 +1,16 @@
 package com.daniel.goncharov.algorithm.playground.tree;
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class IdenticalBinaryTrees {
 
     public int isSameTree(TreeNode leftTree, TreeNode rightTree) {
-        if (leftTree == null && rightTree == null) return 1;
-        if ((leftTree == null && rightTree != null) || (leftTree != null && rightTree == null)) return 0;
-        Deque<TreeNode> leftDeque = new LinkedList<>(Arrays.asList(leftTree));
-        Deque<TreeNode> rightDeque = new LinkedList<>(Arrays.asList(rightTree));
-        while (!leftDeque.isEmpty() && !rightDeque.isEmpty()) {
-            TreeNode currentLeft = leftDeque.poll();
-            TreeNode currentRight = rightDeque.poll();
-            if (currentLeft.val != currentRight.val) return 0;
-            if (currentLeft.left != null) leftDeque.add(currentLeft.left);
-            if (currentLeft.right != null) leftDeque.add(currentLeft.right);
-            if (currentRight.left != null) leftDeque.add(currentRight.left);
-            if (currentRight.right != null) leftDeque.add(currentRight.right);
-            if (leftDeque.size() != rightDeque.size()) return 0;
-        }
-        return 1;
+        return areSameTrees(leftTree, rightTree) ? 1 : 0;
+    }
+
+    private boolean areSameTrees(TreeNode leftTree, TreeNode rightTree) {
+        if (leftTree == null && rightTree == null) return true;
+        if (leftTree == null || rightTree == null) return false;
+        return leftTree.val == rightTree.val
+                && areSameTrees(leftTree.left, rightTree.left)
+                && areSameTrees(leftTree.right, rightTree.right);
     }
 }
