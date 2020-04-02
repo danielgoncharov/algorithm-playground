ab9604794dfce973e67e5da831cb935771d3620f
Danylo Goncharov <danylo.goncharov@xing.com>
2020-04-02 09:09:25 +0200

    SymmetricBinaryTree recursive solution

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/SymmetricBinaryTree.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/SymmetricBinaryTree.java
@@ -1,34 +1,15 @@
 package com.daniel.goncharov.algorithm.playground.tree;
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class SymmetricBinaryTree {
 
     public int isSymmetric(TreeNode root) {
-        Deque<TreeNode> symmetricCheckNodeDeque = new LinkedList<>();
-        Deque<TreeNode> layerDeque = new LinkedList<>(Arrays.asList(root));
-        int nextSize = 2;
-        while (!layerDeque.isEmpty()) {
-            if (layerDeque.size() == nextSize) {
-                while (!symmetricCheckNodeDeque.isEmpty()) {
-                    TreeNode leftCorner = symmetricCheckNodeDeque.removeFirst();
-                    TreeNode rightCorner = symmetricCheckNodeDeque.removeLast();
-                    if (leftCorner == null && rightCorner == null) continue;
-                    if (leftCorner == null || rightCorner == null) throw new RuntimeException("one of them null");
-                    if (leftCorner.val != rightCorner.val)
-                        throw new RuntimeException("vals: " + leftCorner.val + " " + rightCorner.val);
-                }
-                nextSize *= 2;
-            }
-            TreeNode currentTreeNode = layerDeque.poll();
-            if (currentTreeNode == null) continue;
-            layerDeque.add(currentTreeNode.left);
-            layerDeque.add(currentTreeNode.right);
-            symmetricCheckNodeDeque.add(currentTreeNode.left);
-            symmetricCheckNodeDeque.add(currentTreeNode.right);
-        }
-        return 1;
+        return isSymmetricNodes(root.left, root.right) ? 1 : 0;
+    }
+
+    private boolean isSymmetricNodes(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetricNodes(left.right, right.left) && isSymmetricNodes(left.left, right.right);
     }
 }
