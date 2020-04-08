925bb3c5c16ba1447d6d17a7f0677cbe1a151c74
Danylo Goncharov <danylo.goncharov@xing.com>
2020-04-08 21:14:22 +0200

    BST create from inorder

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/SortedArrayToBalancedBST.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/SortedArrayToBalancedBST.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class SortedArrayToBalancedBST {
 
     public TreeNode sortedArrayToBST(List<Integer> array) {
+        if (array.size() == 1) return new TreeNode(array.get(0));
         TreeNode treeNode = new TreeNode(0);
         createBST(treeNode, 0, array.size() - 1, array);
         return treeNode;
@@ -16,17 +17,17 @@ public class SortedArrayToBalancedBST {
             int end,
             List<Integer> array
     ) {
-        int middle = (end + start) / 2;
+        int middle = (end + start + 1) / 2;
         treeNode.val = array.get(middle);
         if (middle - 1 >= start) {
             TreeNode left = new TreeNode(0);
             treeNode.left = left;
-            createBST(treeNode, start, middle - 1, array);
+            createBST(left, start, middle - 1, array);
         }
         if (middle + 1 <= end) {
             TreeNode right = new TreeNode(0);
             treeNode.right = right;
-            createBST(treeNode, middle + 1, end, array);
+            createBST(right, middle + 1, end, array);
         }
     }
 
