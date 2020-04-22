0b9829150b34be82986b6e637d6f34355dc17929
Danylo Goncharov <danylo.goncharov@xing.com>
2020-04-22 21:04:41 +0200

    BSTIterator.java

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/BSTIterator.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/BSTIterator.java
@@ -1,58 +1,34 @@
 package com.daniel.goncharov.algorithm.playground.tree;
 
-import java.util.HashSet;
-import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class BSTIterator {
 
     private Stack<TreeNode> stack = new Stack<>();
-    private HashSet<TreeNode> visitedTreeNode = new HashSet<>();
+    private TreeNode current;
 
     public BSTIterator(TreeNode root) {
-        stack.add(root);
+        current=root;
     }
 
     /**
      * @return whether we have a next smallest number
      */
     public boolean hasNext() {
-        return !stack.isEmpty();
+        return !stack.isEmpty() || current != null;
     }
 
     /**
      * @return the next smallest number
      */
     public int next() {
-        return traverseToNext();
-    }
-
-    private int traverseToNext() {
-        while (!stack.isEmpty()) {
-            TreeNode treeNode = stack.peek();
-
-            if (treeNode.left == null && treeNode.right == null) {
-                visitedTreeNode.add(treeNode);
-                return stack.pop().val;
-            }
-
-            if (treeNode.left == null) {
-                visitedTreeNode.add(treeNode);
-                if (treeNode.right != null) stack.add(treeNode.right);
-                return stack.pop().val;
-            }
-
-            //left is not null from here on
-            if (!visitedTreeNode.contains(treeNode.left)) {
-                stack.add(treeNode.left);
-                continue;
-            }
-
-            visitedTreeNode.remove(treeNode.left);
-            treeNode = stack.pop();
-            if (treeNode.right != null) stack.add(treeNode.right);
-            return treeNode.val;
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
         }
-        throw new NoSuchElementException();
+        current = stack.pop();
+        TreeNode node = current;
+        current = current.right;
+        return node.val;
     }
 }
