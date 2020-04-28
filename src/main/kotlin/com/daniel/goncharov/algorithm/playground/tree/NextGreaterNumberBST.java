aa139cc00d790a0d52e335ecd1a2787519c67455
Danylo Goncharov <danylo.goncharov@xing.com>
2020-04-28 22:09:41 +0200

    Improve solution

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/NextGreaterNumberBST.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/NextGreaterNumberBST.java
@@ -1,6 +1,5 @@
 package com.daniel.goncharov.algorithm.playground.tree;
 
-import java.util.LinkedHashSet;
 import java.util.Stack;
 
 public class NextGreaterNumberBST {
@@ -8,25 +7,17 @@ public class NextGreaterNumberBST {
     public TreeNode getSuccessor(TreeNode root, int anchor) {
 
         Stack<TreeNode> stack = new Stack<>();
-        LinkedHashSet<TreeNode> visitedNodes = new LinkedHashSet<>();
         TreeNode inspectNode = root;
-        while (inspectNode != null) {
-            if (inspectNode.left == null && inspectNode.right == null) {
-                visitedNodes.add(inspectNode);
-                inspectNode = stack.isEmpty() ? null : stack.pop();
-            } else if (inspectNode.left != null && !visitedNodes.contains(inspectNode.left)) {
-                if (inspectNode.right != null) stack.add(inspectNode.right);
-                stack.add(inspectNode);
-                inspectNode = inspectNode.left;
-            } else if (inspectNode.left != null && visitedNodes.contains(inspectNode.left)) {
-                visitedNodes.add(inspectNode);
-                inspectNode = stack.isEmpty() ? null : stack.pop();
-            } else if (inspectNode.left == null && inspectNode.right != null) {
-                visitedNodes.add(inspectNode);
-                inspectNode = inspectNode.right;
+        while (!stack.isEmpty() || inspectNode != null) {
+            TreeNode topOfTheStack = inspectNode;
+            while (topOfTheStack != null) {
+                stack.add(topOfTheStack);
+                topOfTheStack = topOfTheStack.left;
             }
+            topOfTheStack = stack.pop();
+            if (topOfTheStack.val > anchor) return topOfTheStack;
+            inspectNode = topOfTheStack.right;
         }
-        for (TreeNode treeNode : visitedNodes) if (treeNode.val > anchor) return treeNode;
         return null;
     }
 
