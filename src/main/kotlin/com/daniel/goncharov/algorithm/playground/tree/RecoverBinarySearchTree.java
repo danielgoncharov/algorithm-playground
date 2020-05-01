ad77358be8ac556872f17577d07c133a894f6d3e
Danylo Goncharov <danylo.goncharov@xing.com>
2020-05-01 09:06:52 +0200

    count adjusent

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/RecoverBinarySearchTree.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/RecoverBinarySearchTree.java
@@ -1,6 +1,7 @@
 package com.daniel.goncharov.algorithm.playground.tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 public class RecoverBinarySearchTree {
@@ -9,19 +10,26 @@ public class RecoverBinarySearchTree {
         TreeNode currentElement = root;
         Stack<TreeNode> stack = new Stack<>();
         ArrayList<Integer> result = new ArrayList<>();
-        Integer previousValue = null;
+        int previousValue = Integer.MIN_VALUE;
+        TreeNode adjusent = null;
         while (!stack.isEmpty() || currentElement != null || result.size() == 2) {
             while (currentElement != null) {
                 stack.add(currentElement);
                 currentElement = currentElement.left;
             }
+            if (stack.isEmpty()) break;
             currentElement = stack.pop();
-            if (previousValue != null && previousValue.compareTo(currentElement.val) == 1) {
+            if (previousValue > currentElement.val) {
                 result.add(previousValue);
+                adjusent = currentElement;
             }
             previousValue = currentElement.val;
             currentElement = currentElement.right;
         }
+        if (result.size() < 2 && adjusent != null) {
+            result.add(adjusent.val);
+        }
+        Collections.sort(result);
         return result;
     }
 
