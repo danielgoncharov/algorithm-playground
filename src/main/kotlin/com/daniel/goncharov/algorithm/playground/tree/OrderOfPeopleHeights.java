60afac9ad7b536ead4db2c034606572a03b7c731
Danylo Goncharov <danylo.goncharov@xing.com>
2020-05-31 20:29:38 +0200

    OrderOfPeopleHeights wip

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/tree/OrderOfPeopleHeights.java
@@ -0,0 +1,39 @@
+package com.daniel.goncharov.algorithm.playground.tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class OrderOfPeopleHeights {
+
+    public ArrayList<Integer> order(ArrayList<Integer> heights, ArrayList<Integer> inFronts) {
+        SegmentTree segmentTree = new SegmentTree(new int[heights.size()]);
+        ArrayList<Item> items = createSortedItemsList(heights, inFronts);
+        for (int index = 0; index < items.size(); index++) {
+            segmentTree.getSum()
+        }
+    }
+
+    private ArrayList<Item> createSortedItemsList(ArrayList<Integer> heights, ArrayList<Integer> inFronts) {
+        ArrayList<Item> result = new ArrayList<>();
+        for (int index = 0; index < heights.size(); index++) {
+            result.add(new Item(heights.get(index), inFronts.get(index)));
+        }
+        Collections.sort(result, Comparator.comparing(item -> item.height));
+        return result;
+    }
+
+
+
+    private class Item {
+        final Integer height;
+        final Integer peopleInFront;
+
+        public Item(Integer height, Integer peopleInFront) {
+            this.height = height;
+            this.peopleInFront = peopleInFront;
+        }
+    }
+}
+
+
