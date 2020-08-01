5f48db99dc5e4dd2d3264df53e1b888316efed7e
Danylo Goncharov <danylo.goncharov@xing.com>
2020-08-01 18:29:22 +0200

    LongestPalindromicSubsequence.java

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/LongestPalindromicSubsequence.java
@@ -0,0 +1,26 @@
+package com.daniel.goncharov.algorithm.playground.dp;
+
+public class LongestPalindromicSubsequence {
+
+    public int solve(String inputString) {
+        String a = inputString;
+        String b = new StringBuilder(inputString).reverse().toString();
+        int[][] table = new int[a.length() + 1][b.length() + 1];
+        for (int i = 0; i < table.length; i++) {
+            int row[] = table[i];
+            for (int j = 0; j < row.length; j++) {
+                if (i == 0 || j == 0) {
+                    row[j] = 0;
+                    continue;
+                }
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    row[j] = table[i - 1][j - 1] + 1;
+                    continue;
+                } else {
+                    row[j] = Math.max(table[i - 1][j], table[i][j - 1]);
+                }
+            }
+        }
+        return table[a.length()][b.length()];
+    }
+}
