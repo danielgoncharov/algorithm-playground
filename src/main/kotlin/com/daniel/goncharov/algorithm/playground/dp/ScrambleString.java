67d1db00ee29e095dc3eacb19584f0f28cb9d66c
Danylo Goncharov <danylo.goncharov@xing.com>
2020-10-01 19:58:01 +0200

    ScrambleString recursive

---
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/ScrambleString.java
@@ -0,0 +1,18 @@
+package com.daniel.goncharov.algorithm.playground.dp;
+
+public class ScrambleString {
+
+    public int isScramble(final String A, final String B) {
+        return isScrambleRec(A, B) ? 1 : 0;
+    }
+
+    private boolean isScrambleRec(final String A, final String B) {
+        if (A.length() != B.length()) return false;
+        int length = A.length();
+        if (length == 0) return true;
+        if (length == 1) return A.charAt(0) == B.charAt(0);
+
+        return isScrambleRec(A.substring(0, length / 2), B.substring(0, length / 2)) ||
+                isScrambleRec(A.substring(0, length / 2), B.substring(length / 2, length));
+    }
+}
