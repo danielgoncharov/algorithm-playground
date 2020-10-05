13fe258edeb4fa40bb2b1cfc4f6c21616708ac63
Danylo Goncharov <danylo.goncharov@xing.com>
2020-10-05 20:50:46 +0200

    ScrambleString

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/ScrambleString.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/ScrambleString.java
@@ -1,64 +1,42 @@
 package com.daniel.goncharov.algorithm.playground.dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ScrambleString {
 
     public int isScramble(final String A, final String B) {
         if (A.length() != B.length()) return 0;
-        return isScrambleRec(A, B, 0, 0, A.length()) ? 1 : 0;
+        HashMap<String, Boolean> cache = new HashMap<>();
+        return isScrambleRec(A, B, cache) ? 1 : 0;
     }
 
-    private boolean isScrambleRec(
-            String A,
-            String B,
-            int startA,
-            int startB,
-            int length
-    ) {
-        if (length == 0) return true;
-        if (length == 1) return A.charAt(startA) == B.charAt(startB);
+    private boolean isScrambleRec(String s1, String s2, Map<String, Boolean> cache) {
 
+        if (s1.equals(s2)) return true;
+        if (s1.length() <= 1) return false;
+        String cacheKey = s1 + " " + s2;
+        if (cache.containsKey(cacheKey)) return cache.get(cacheKey);
 
-        for (int index = startA; index < A.length(); index++) {
+        boolean flag = false;
+        int n = s1.length();
+        for (int i = 1; i < n; i++) {
+            if (isScrambleRec(s1.substring(0, i), s2.substring(0, i), cache)
+                    && isScrambleRec(s1.substring(i), s2.substring(i), cache)) {
+                flag = true;
+                break;
+            }
 
-
-            if ((
-                    isScrambleRec(
-                            A,
-                            B,
-                            startA,
-                            startB,
-                            index + 1
-                    ) &&
-                            isScrambleRec(
-                                    A,
-                                    B,
-                                    index + 1,
-                                    index + 1,
-                                    length - index
-                            )
-            )
-                    ||
-                    (
-                            isScrambleRec(
-                                    A,
-                                    B,
-                                    startA,
-                                    length - index - 1,
-                                    index + 1
-                            ) &&
-                                    isScrambleRec(
-                                            A,
-                                            B,
-                                            length - index - 1,
-                                            startB,
-                                            index + 1
-                                    )
-
-                    )) return true;
-
+            if (isScrambleRec(s1.substring(0, i), s2.substring(n - i, n), cache)
+                    && isScrambleRec(s1.substring(i, n), s2.substring(0, n - i), cache)) {
+                flag = true;
+                break;
+            }
         }
 
-        return false;
-
+        cache.put(cacheKey, flag);
+        return flag;
     }
+
 }
+
