6c63a64aef7509491c2ed267e8a2f357fcc9e1b8
Danylo Goncharov <danylo.goncharov@xing.com>
2020-10-18 12:01:59 +0200

    Top down approach

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/WaysToDecode.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/WaysToDecode.java
@@ -1,26 +1,39 @@
 package com.daniel.goncharov.algorithm.playground.dp;
 
+import java.util.HashMap;
+
 public class WaysToDecode {
 
     private int numDecodings = 0;
 
     public int numDecodings(String encodedString) {
         if (encodedString.length() == 0) return 0;
-        return countDecodings(encodedString, 0, 1);
+        return countDecodings(encodedString, 0, 1, new HashMap<>());
     }
 
     private int countDecodings(
             String encodedString,
             int left,
-            int right
+            int right,
+            HashMap<String, Integer> cache
     ) {
+
         int value = Integer.valueOf(encodedString.substring(left, right));
         if (value < 27) return 0;
         if (right == encodedString.length()) return 1;
+        String key = new StringBuilder().append(left).append(right).toString();
+        if (cache.containsKey(key)) return cache.get(cache);
 
 
-        return countDecodings(encodedString, left, right + 1) +
-                countDecodings(encodedString, right, right + 1);
+        int contiguesResult = countDecodings(encodedString, left, right + 1, cache);
+        key = new StringBuilder().append(left).append(right + 1).toString();
+        cache.put(key, contiguesResult);
+
+        int nextIsSeparateResult = countDecodings(encodedString, right, right + 1, cache);
+        key = new StringBuilder().append(right).append(right + 1).toString();
+        cache.put(key, nextIsSeparateResult);
+
+        return contiguesResult + nextIsSeparateResult;
 
     }
 
