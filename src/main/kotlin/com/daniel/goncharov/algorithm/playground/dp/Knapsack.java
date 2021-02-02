fc749ff4d999da6a4220f47b3840117defe1ed2b
Danylo Goncharov <danylo.goncharov@xing.com>
2021-02-02 20:00:41 +0100

    Knapsack bottom up

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/Knapsack.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/Knapsack.java
@@ -5,7 +5,34 @@ import java.util.ArrayList;
 public class Knapsack {
 
     public int solve(ArrayList<Integer> values, ArrayList<Integer> weights, int capacity) {
-        return solveRecursive(values, weights, capacity, values.size() - 1);
+        return solveBottomUp(values, weights, capacity);
+    }
+
+    private int solveBottomUp(ArrayList<Integer> values,
+                              ArrayList<Integer> weights,
+                              int capacity) {
+        int[][] cache = new int[weights.size() + 1][capacity + 1];
+        int result = 0;
+        for (int i = 0; i < cache.length; i++) {
+            int[] row = cache[i];
+            for (int weight = 0; weight < row.length; weight++) {
+                if (i == 0 || weight == 0) {
+                    row[weight] = 0;
+                    continue;
+                }
+                int currentWeight = weights.get(i - 1);
+                if (currentWeight > weight) {
+                    row[weight] = cache[i - 1][weight];
+                } else {
+                    row[weight] = Math.max(
+                            cache[i - 1][weight],
+                            values.get(i - 1) + cache[i - 1][weight - currentWeight]
+                    );
+                }
+                result = row[weight];
+            }
+        }
+        return result;
     }
 
     private int solveRecursive(ArrayList<Integer> values,
