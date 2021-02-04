b3d675b6e53cf20920fe8990aeb9d1cf758cf645
Danylo Goncharov <danylo.goncharov@xing.com>
2021-02-04 19:36:40 +0100

    Memorization

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/UniqueBinarySearchTrees.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/UniqueBinarySearchTrees.java
@@ -1,22 +1,32 @@
 package com.daniel.goncharov.algorithm.playground.dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UniqueBinarySearchTrees {
 
     public int numTrees(int A) {
-        return numTreesRecursive(1, A);
+        Map<Integer, Integer> map = new HashMap<>();
+        return numTreesRecursive(1, A, map);
     }
 
     private int numTreesRecursive(
             int left,
-            int right
+            int right,
+            Map<Integer, Integer> map
     ) {
+        int numberOfElements = right - left + 1;
+        if (map.containsKey(numberOfElements)) {
+            return map.get(numberOfElements);
+        }
         if (left >= right) {
             return 1;
         }
         int sum = 0;
         for (int i = left; i <= right; i++) {
-            sum += numTreesRecursive(left, i - 1) * numTreesRecursive(i + 1, right);
+            sum += numTreesRecursive(left, i - 1, map) * numTreesRecursive(i + 1, right, map);
         }
+        map.put(numberOfElements, sum);
         return sum;
     }
 }
