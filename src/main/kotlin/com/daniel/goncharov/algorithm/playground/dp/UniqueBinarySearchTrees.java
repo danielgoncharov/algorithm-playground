70b898b3e157adff97209d90d4463a13cc4bd27f
Danylo Goncharov <danylo.goncharov@xing.com>
2021-02-04 19:12:02 +0100

    Correct recursive

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/UniqueBinarySearchTrees.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/UniqueBinarySearchTrees.java
@@ -3,7 +3,6 @@ package com.daniel.goncharov.algorithm.playground.dp;
 public class UniqueBinarySearchTrees {
 
     public int numTrees(int A) {
-        if (A == 2) return 2;
         return numTreesRecursive(1, A);
     }
 
@@ -11,12 +10,12 @@ public class UniqueBinarySearchTrees {
             int left,
             int right
     ) {
-        if (left > right || left == 0 || right - left == 1) {
+        if (left >= right) {
             return 1;
         }
         int sum = 0;
         for (int i = left; i <= right; i++) {
-            sum += numTreesRecursive(left - 1, i) * numTreesRecursive(i + 1, right);
+            sum += numTreesRecursive(left, i - 1) * numTreesRecursive(i + 1, right);
         }
         return sum;
     }
