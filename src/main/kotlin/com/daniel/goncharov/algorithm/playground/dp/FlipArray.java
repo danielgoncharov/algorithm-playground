e5b1efbc0bfb0a4d06707d84b2658c74b53915ba
Danylo Goncharov <danylo.goncharov@xing.com>
2021-02-23 20:22:10 +0100

    FlipArray

--- src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/FlipArray.java
+++ src/main/kotlin/com/daniel/goncharov/algorithm/playground/dp/FlipArray.java
@@ -1,41 +1,32 @@
 package com.daniel.goncharov.algorithm.playground.dp;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class FlipArray {
 
-    public int solve(List<Integer> a) {
-        List<Integer> numbers = new ArrayList<>(a);
-        Collections.sort(numbers);
+    public int solve(final List<Integer> a) {
+        int numbers[] = new int[a.size()];
         int sum = 0;
-        for (Integer i : numbers) sum += i;
-        int[][] cache = new int[numbers.size() + 1][sum / 2 + 1];
-        int[][] minSum = new int[numbers.size() + 1][sum / 2 + 1];
-        for (int i = 0; i < cache.length; i++) {
-            int[] column = cache[i];
-            for (int j = 0; j < column.length; j++) {
-                if (i == 0 || j == 0) {
-                    cache[i][j] = 0;
-                    minSum[i][j] = 0;
-                    continue;
+        for (int i = 0; i < numbers.length; i++) sum += numbers[i];
+        int dp[] = new int[sum / 2 + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = sum / 2; j >= 0; j--) {
+                if (j - numbers[i] >= 0 && dp[j - numbers[i]] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[j - numbers[i]] + 1);
                 }
-                int reminder = j - numbers.get(i - 1);
-                if (reminder < 0) {
-                    cache[i][j] = cache[i - 1][j];
-                    minSum[i][j] = minSum[i - 1][j];
-                } else if (minSum[i - 1][j] > numbers.get(i - 1) + minSum[i - 1][reminder]) {
-                    cache[i][j] = cache[i - 1][j];
-                    minSum[i][j] = minSum[i - 1][j];
-                } else {
-                    cache[i][j] = 1 + cache[i - 1][reminder];
-                    minSum[i][j] = numbers.get(i - 1) + minSum[i - 1][reminder];
-                }
-
             }
         }
-        return cache[numbers.size()][sum / 2];
+
+        for (int i = dp.length - 1; i >= 0; i--) {
+            if (dp[i] != Integer.MAX_VALUE) {
+                return dp[i];
+            }
+        }
+
+        return 0;
     }
 
 }
