371d122ff9ad80634fff53380a869cd1742a279b
danylo.goncharov <danylo.goncharov@xing.com>
2021-07-28 21:02:50 +0200

    GasStation

---
+++ src/main/java/com/daniel/goncharov/algorithm/playground/greedy/GasStation.java
@@ -0,0 +1,24 @@
+package com.daniel.goncharov.algorithm.playground.greedy;
+
+import java.util.List;
+
+public class GasStation {
+
+    public int canCompleteCircuit(final List<Integer> gas, final List<Integer> cost) {
+        int currentFuel = 0;
+        int remaining;
+        int total = 0;
+        int start = 0;
+        for(int i = 0; i < gas.size(); i++){
+            remaining = gas.get(i) - cost.get(i);
+            if(currentFuel >= 0)
+                currentFuel += remaining;
+            else{
+                currentFuel = remaining;
+                start = i;
+            }
+            total += remaining;
+        }
+        return total >= 0 ? start : -1;
+    }
+}
