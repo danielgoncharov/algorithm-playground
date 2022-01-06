b94cb8806f3f1ad0cf4f52c42d98676164b403da
danylo.goncharov <danylo.goncharov@xing.com>
2022-01-06 20:21:48 +0100

    small optimisation

--- src/main/java/com/daniel/goncharov/algorithm/playground/graph/SmallestMultipleWith0and1.java
+++ src/main/java/com/daniel/goncharov/algorithm/playground/graph/SmallestMultipleWith0and1.java
@@ -1,12 +1,15 @@
 package com.daniel.goncharov.algorithm.playground.graph;
 
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class SmallestMultipleWith0and1 {
     // https://www.youtube.com/watch?v=sdTubUR99OA
     public String multiple(int number) {
         Deque<Item> deque = new LinkedList<>();
+        Set<Integer> reminders = new HashSet<>();
         if (1 % number == 0) return "1";
         deque.add(new Item("1", 1 % number));
         while (!deque.isEmpty()) {
@@ -16,12 +19,20 @@ public class SmallestMultipleWith0and1 {
             String next = item.number + "0";
             int thisModZero = item.previousMod * 10 % number;
             if (thisModZero  == 0) return next;
-            deque.addLast(new Item(next, thisModZero));
+            if(!reminders.contains(thisModZero)){
+                deque.addLast(new Item(next, thisModZero));
+                reminders.add(thisModZero);
+            }
 
             next = item.number + "1";
             int thisModOne = (item.previousMod * 10 + 1)% number;
             if (thisModOne  == 0) return next;
-            deque.addLast(new Item(next, thisModOne));
+
+            if(!reminders.contains(thisModOne)){
+                deque.addLast(new Item(next, thisModOne));
+                reminders.add(thisModOne);
+            }
+
         }
         return "-1";
     }
