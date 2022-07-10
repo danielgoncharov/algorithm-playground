e8201191ef15db762d28c4906922999cb1175237
danylo.goncharov <danylo.goncharov@xing.com>
2022-07-10 09:06:39 +0200

    top to bottom optimised

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/JumpGame2.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/JumpGame2.kt
@@ -3,16 +3,31 @@ package com.daniel.goncharov.algorithm.playground.leetcode.dp
 class JumpGame2 {
 
     fun canJump(nums: IntArray): Boolean {
-        return canJump(0, nums)
+        val cache = MutableList(
+            nums.size
+        ) { index ->
+            if (index == 0) {
+                true
+            } else {
+                null
+            }
+        }
+        return canJump(0, nums, cache)
     }
 
-    private fun canJump(start: Int, nums: IntArray): Boolean {
+    private fun canJump(
+        start: Int,
+        nums: IntArray,
+        steps: MutableList<Boolean?>
+    ): Boolean {
+        steps[start]?.let { canJump -> return canJump }
         if (start >= nums.size - 1) return true
         if (nums[start] == 0) return false
         var result = false
         for (index in start + 1 until nums[start] + start + 1) {
-            result = result or canJump(index, nums)
+            result = result or canJump(index, nums, steps)
         }
+        steps[start] = result
         return result
     }
 
