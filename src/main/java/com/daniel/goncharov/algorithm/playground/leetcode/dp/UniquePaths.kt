59b4fd44a761fcd71f6e8cd9b15c511685f6264a
danylo.goncharov <danylo.goncharov@xing.com>
2022-07-21 19:39:20 +0200

    top to bottom

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/UniquePaths.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/UniquePaths.kt
@@ -3,27 +3,33 @@ package com.daniel.goncharov.algorithm.playground.leetcode.dp
 class UniquePaths {
 
     fun uniquePaths(rows: Int, columns: Int): Int {
-        return countPaths(0, 0, rows, columns)
+        val steps = MutableList(rows) { MutableList(columns) { -1 } }
+        return countPaths(0, 0, rows, columns, steps)
     }
 
     private fun countPaths(
         currentRowIndex: Int,
         currentColumnIndex: Int,
         rows: Int,
-        columns: Int
+        columns: Int,
+        steps: MutableList<MutableList<Int>>
     ): Int {
         if (currentRowIndex == rows || currentColumnIndex == columns) return 0
         if (currentRowIndex == rows - 1 && currentColumnIndex == columns - 1) return 1
-        return countPaths(
+        if (steps[currentRowIndex][currentColumnIndex] != -1) return steps[currentRowIndex][currentColumnIndex]
+        steps[currentRowIndex][currentColumnIndex] = countPaths(
             currentRowIndex + 1,//go down
             currentColumnIndex,
             rows,
-            columns
+            columns,
+            steps,
         ) + countPaths(//
             currentRowIndex,
             currentColumnIndex + 1,//go right
             rows,
-            columns
+            columns,
+            steps
         )
+        return steps[currentRowIndex][currentColumnIndex]
     }
 }
