6a4c761135f67c22628382e1d1d96e2f6eb9c3c2
danylo.goncharov <danylo.goncharov@xing.com>
2022-07-26 20:44:11 +0200

    not really smart solution

---
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/KConcatenationMaximumSum.kt
@@ -0,0 +1,48 @@
+package com.daniel.goncharov.algorithm.playground.leetcode.dp
+
+import kotlin.math.max
+
+class KConcatenationMaximumSum {
+
+    fun kConcatenationMaxSum(arr: IntArray, repeatTime: Int): Int {
+        if (arr.isEmpty()) return 0
+        val maxPrefix = maxPrefix(arr)
+        val wholeArray = arr.sum()
+        val maxSubArray = maxSubArray(arr)
+        arr.reverse()
+        val maxSufix = maxPrefix(arr)
+
+        return arrayListOf(
+            maxPrefix + maxSufix,
+            wholeArray * repeatTime,
+            maxSubArray,
+            0
+        ).maxOrNull() ?: 0
+    }
+
+    fun maxPrefix(nums: IntArray): Int {
+        var localMax = nums.first()
+        for (index in 1 until nums.size) {
+            val number = nums[index]
+            if (number >= localMax + number) {
+                return localMax
+            }
+        }
+        return localMax
+    }
+
+    fun maxSubArray(nums: IntArray): Int {
+        var max = nums.first()
+        var localMax = nums.first()
+        for (index in 1 until nums.size) {
+            val number = nums[index]
+            localMax = max(number, localMax + number)
+            if (localMax > max) {
+                max = localMax
+            }
+        }
+        return max
+    }
+}
+
+
