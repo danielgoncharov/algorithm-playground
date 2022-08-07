4097d27857fe3ec079aef0830d3973aecdc66fe2
danylo.goncharov <danylo.goncharov@xing.com>
2022-08-07 11:07:54 +0200

    MaximumEarningsFromTaxi top to bottom

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/MaximumEarningsFromTaxi.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/MaximumEarningsFromTaxi.kt
@@ -3,22 +3,31 @@ package com.daniel.goncharov.algorithm.playground.leetcode.dp
 class MaximumEarningsFromTaxi {
 
     fun maxTaxiEarnings(endStop: Int, rides: Array<IntArray>): Long {
+        val cache = HashMap<Int, Long>()
         val typedRides = rides.map { ride -> Ride(ride[0], ride[1], ride[2]) }
-        return calculateEarning(StartRide, endStop, typedRides)
+        return calculateEarning(StartRide, typedRides, cache)
     }
 
-    private fun calculateEarning(currentRide: Ride, endStop: Int, rides: List<Ride>): Long {
+    private fun calculateEarning(
+        currentRide: Ride,
+        rides: List<Ride>,
+        cache: MutableMap<Int, Long>
+    ): Long {
+        if (cache.containsKey(currentRide.start)) {
+            return cache[currentRide.start] ?: 0
+        }
         val maxLaterRideProfit = rides
             .filter { rideToFilter -> rideToFilter.start >= currentRide.end }
             .map { newRide ->
                 calculateEarning(
                     newRide,
-                    endStop,
-                    rides
+                    rides,
+                    cache
                 )
             }
             .maxOrNull() ?: 0L
-        return currentRide.profit + maxLaterRideProfit
+        cache[currentRide.start] = currentRide.profit + maxLaterRideProfit
+        return cache[currentRide.start] ?: 0
     }
 }
 
