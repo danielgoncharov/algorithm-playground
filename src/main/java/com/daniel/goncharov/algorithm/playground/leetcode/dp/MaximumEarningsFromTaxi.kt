4a39e7e4bc6a6379c2a40611827d0736bab3b34d
danylo.goncharov <danylo.goncharov@xing.com>
2022-08-07 10:55:32 +0200

    brute force correct

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/MaximumEarningsFromTaxi.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/MaximumEarningsFromTaxi.kt
@@ -1,35 +1,30 @@
 package com.daniel.goncharov.algorithm.playground.leetcode.dp
 
-import kotlin.math.max
-
 class MaximumEarningsFromTaxi {
 
     fun maxTaxiEarnings(endStop: Int, rides: Array<IntArray>): Long {
-        var maxEarnings = Long.MIN_VALUE
         val typedRides = rides.map { ride -> Ride(ride[0], ride[1], ride[2]) }
-        typedRides.forEach { startRide ->
-            maxEarnings = max(
-                maxEarnings,
-                calculateEarning(startRide, endStop, typedRides)
-            )
-        }
-        return maxEarnings
+        return calculateEarning(StartRide, endStop, typedRides)
     }
 
-    private fun calculateEarning(ride: Ride, endStop: Int, rides: List<Ride>): Long {
-        val rideToStart = rides.filter { rideToFilter -> ride.end == rideToFilter.start }
-
-        return (rideToStart.map { newRide ->
-            calculateEarning(
-                newRide,
-                endStop,
-                rides
-            )
-        }
-            .maxOrNull() ?: 0L) + ride.profit
+    private fun calculateEarning(currentRide: Ride, endStop: Int, rides: List<Ride>): Long {
+        val maxLaterRideProfit = rides
+            .filter { rideToFilter -> rideToFilter.start >= currentRide.end }
+            .map { newRide ->
+                calculateEarning(
+                    newRide,
+                    endStop,
+                    rides
+                )
+            }
+            .maxOrNull() ?: 0L
+        return currentRide.profit + maxLaterRideProfit
     }
 }
 
 data class Ride(val start: Int, val end: Int, val tip: Int) {
     val profit = end - start + tip
+
 }
+
+val StartRide = Ride(0, 0, 0)
