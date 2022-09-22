ee03cc9cb22f473284cbee78c8bcdfc6dcf4f4b6
danylo.goncharov <danylo.goncharov@xing.com>
2022-09-22 20:34:29 +0200

    LongestPalindromicSubsequence bottom up

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/LongestPalindromicSubsequence.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/LongestPalindromicSubsequence.kt
@@ -5,36 +5,31 @@ import kotlin.math.max
 class LongestPalindromicSubsequence {
 
     fun longestPalindromeSubseq(seq: String): Int {
-        val cache = MutableList(seq.length) {
-            MutableList(seq.length) { 0 }
+        val cache = MutableList(seq.length) { row ->
+            MutableList(seq.length) { column ->
+                if (row == column) 1
+                else 0
+            }
         }
-        return find(seq, 0, seq.length - 1, cache)
+        for (startColumn in 1 until seq.length) {
+            for (row in 0 until seq.length - startColumn) {
+                val column = startColumn + row
+                val left = seq[row]
+                val right = seq[column]
+                when {
+                    startColumn == 1 && left == right -> cache[row][column] = 2
+                    left == right -> cache[row][column] = 2 + cache[row + 1][column - 1]
+                    else -> {
+                        cache[row][column] = max(
+                            cache[row][column - 1],
+                            cache[row + 1][column]
+                        )
+                    }
+                }
+
+            }
+        }
+        return cache[0][seq.length - 1]
     }
 
-    fun find(
-        seq: String,
-        left: Int,
-        right: Int,
-        cache: MutableList<MutableList<Int>>
-    ): Int {
-        if (left == right) {
-            cache[left][right] = 1
-        } else if (seq[left] == seq[right] && left + 1 == right) {
-            cache[left][right] = 2
-        } else if (seq[left] == seq[right]) {
-            cache[left][right] = find(
-                seq,
-                left + 1,
-                right - 1,
-                cache
-            ) + 2
-        } else {
-            cache[left][right] = max(
-                find(seq, left, right - 1, cache),
-                find(seq, left + 1, right, cache)
-            )
-        }
-        return cache[left][right]
-
-    }
 }
