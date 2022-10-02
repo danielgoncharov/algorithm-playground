6835ead734f072982637c8251b8ff7cebe75aa5d
danylo.goncharov <danylo.goncharov@xing.com>
2022-10-02 08:53:47 +0200

    top down solution

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/UncrossedLines.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/UncrossedLines.kt
@@ -7,24 +7,38 @@ class UncrossedLines {
     // are there any duplicates?
 
     fun maxUncrossedLines(arrayA: IntArray, arrayB: IntArray): Int {
-        return findMaxLines(arrayA, 0, arrayB, 0)
+        val cache: MutableList<MutableList<Int?>> = MutableList(arrayA.size) {
+            MutableList<Int?>(arrayB.size) {
+                null
+            }
+        }
+        return findMaxLines(
+            arrayA,
+            0,
+            arrayB,
+            0,
+            cache
+        )
     }
 
     private fun findMaxLines(
         arrayA: IntArray,
         aIndex: Int,
         arrayB: IntArray,
-        bIndex: Int
+        bIndex: Int,
+        cache: MutableList<MutableList<Int?>>
     ): Int {
         if (aIndex == arrayA.size || bIndex == arrayB.size) return 0
-        return if (arrayA[aIndex] == arrayB[bIndex]) {
-            1 + findMaxLines(arrayA, aIndex + 1, arrayB, bIndex + 1)
+        cache[aIndex][bIndex]?.let { return it }
+        if (arrayA[aIndex] == arrayB[bIndex]) {
+            cache[aIndex][bIndex] = 1 + findMaxLines(arrayA, aIndex + 1, arrayB, bIndex + 1, cache)
         } else {
-            max(
-                findMaxLines(arrayA, aIndex + 1, arrayB, bIndex),
-                findMaxLines(arrayA, aIndex, arrayB, bIndex + 1)
+            cache[aIndex][bIndex] = max(
+                findMaxLines(arrayA, aIndex + 1, arrayB, bIndex, cache),
+                findMaxLines(arrayA, aIndex, arrayB, bIndex + 1, cache)
             )
         }
+        return cache[aIndex][bIndex] ?: 0
     }
 
 }
