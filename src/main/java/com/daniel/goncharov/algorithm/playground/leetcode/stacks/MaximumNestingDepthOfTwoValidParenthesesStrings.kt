e0a542ee6b05fe64a8e2f4913fd055cfc19db013
Danylo Goncharov <danylo.goncharov@xing.com>
2022-10-27 20:24:18 +0200

    correct solution

--- src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/stacks/MaximumNestingDepthOfTwoValidParenthesesStrings.kt
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/stacks/MaximumNestingDepthOfTwoValidParenthesesStrings.kt
@@ -5,36 +5,35 @@ import java.util.*
 class MaximumNestingDepthOfTwoValidParenthesesStrings {
 
     fun maxDepthAfterSplit(seq: String): IntArray {
-        //using stack check if the string is valid vps. use stack for saving index + parenteses, push and pop until empty. Once empty
-        // construct block, split block in half and assign A and B. Process rest of the string
-        val stack = Stack<Char>()
-        var start = 0
+        val stack = Stack<StackElement>()
         val resultList = MutableList(seq.length) { 0 }
         for (index in seq.indices) {
             val character = seq[index]
-            if (character == ')') {
+            val element = if (character == ')') {
                 stack.pop()
             } else {
-                stack.add(character)
+                val element = StackElement.createOpening(stack)
+                stack.add(element)
+                element
             }
-            if (stack.isEmpty()) {
-                val subseq = seq.subSequence(start, index + 1)
-                annotateVps(subseq, start, index, resultList)
-                start = index + 1
-            }
-
+            resultList[index] = element.level % 2
         }
         return resultList.toIntArray()
     }
 
-    private fun annotateVps(
-        subseq: CharSequence,
-        start: Int,
-        index: Int,
-        resultList: MutableList<Int>
-    ) {
-
-
+    data class StackElement(val level: Int, val character: Char) {
+        companion object Apply {
+            fun createOpening(stack: Stack<StackElement>): StackElement {
+                return if (stack.isEmpty()) {
+                    StackElement(0, '(')
+                } else if (stack.peek().character == ')') {
+                    StackElement(stack.peek().level, '(')
+                } else {
+                    StackElement(stack.peek().level + 1, '(')
+                }
+            }
+        }
     }
+
 
 }
