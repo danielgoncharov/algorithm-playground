ad490f767181d0ea5c023bb64fb7876dbc0a2f34
Danylo Goncharov <danylo.goncharov@xing.com>
2022-11-28 19:53:20 +0100

    brute force

---
+++ src/main/java/com/daniel/goncharov/algorithm/playground/leetcode/dp/PerfectSquares.kt
@@ -0,0 +1,32 @@
+package com.daniel.goncharov.algorithm.playground.leetcode.dp
+
+import kotlin.math.min
+import kotlin.math.sqrt
+import kotlin.math.ulp
+
+
+class PerfectSquares {
+
+    fun numSquares(n: Int): Int {
+
+    }
+
+    private fun leastNumSquares(number: Int): Int {
+        if (number == 0) return 0
+
+        val root = sqrt(number.toFloat())
+            .toString()
+            .split(".")
+        val rootInt = root.first().toInt()
+        val rootDec = root.last().toInt()
+
+        if (rootDec == 0) return 1
+
+        var min = Int.MAX_VALUE
+        for (root in rootInt downTo 1) {
+            var remainingNumber = number - root * root
+            min = min(min, leastNumSquares(remainingNumber))
+        }
+        return min
+    }
+}
